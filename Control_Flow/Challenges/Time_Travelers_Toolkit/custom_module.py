7c745a9cce840a7097b5da6ee7184d9b61238fc8
jpaola <jpaolababy@gmail.com>
2024-09-11 23:48:53 -0400

    Adding challenge for Modules

---
+++ Control_Flow/Challenges/Time_Travelers_Toolkit/custom_module.py
@@ -0,0 +1,2 @@
+def generate_time_travel_message(year, destination, cost):
+    return f"Pack your bags! You're traveling to {destination} in the year {year}. The cost of this trip will be ${cost}"
---
+++ Control_Flow/Challenges/Time_Travelers_Toolkit/time_travelers_toolkit.py
@@ -0,0 +1,18 @@
+"""
+In this project, you’ll create a Python script to simulate the experience of time travel using various Python
+modules. You’ll start by building a custom module, then work with dates and times, perform precise calculations,
+implement random selection features, and finally, generate a personalized time travel message.
+"""
+import random
+import custom_module
+import datetime  as dt
+from decimal import Decimal
+
+todays_date = dt.datetime.now()
+base_cost = Decimal('1000.00')
+time_travel_cost = abs(base_cost - Decimal('100.00'))
+final_cost = f"{time_travel_cost: .2f}"
+random_year =random.randint(1800, 2025)
+destination = random.choice(["France", "Egypt", "China", "North America", "Africa", "Russia", "Japan", "Central America", "Korea", "South America", "Australia"])
+
+print(custom_module.generate_time_travel_message(year=random_year, destination=destination, cost=final_cost))
