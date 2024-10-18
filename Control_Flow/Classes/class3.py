07dcdb2bcd55daa28c0a5dab25093dcd190feca3
jpaola <jpaolababy@gmail.com>
2024-10-18 00:14:39 -0400

    Adding modifications to class3.py in Control Flow

--- Control_Flow/Classes/class3.py
+++ Control_Flow/Classes/class3.py
@@ -2,9 +2,21 @@ class Circle:
     pi = 3.14
 
     def __init__(self, diameter):
-        self.diameter = diameter
-        print(f"New circle with diameter: {self.diameter}")
+        print("Creating circle with diameter {d}".format(d=diameter))
+        # Add assignment for self.radius here:
+        self.radius = diameter / 2
+
+    def circumference(self):
+        circumference = 2 * self.pi * self.radius
+        return circumference
 
 
-# Create a circle teaching_table with diameter 36
+medium_pizza = Circle(12)
 teaching_table = Circle(36)
+round_room = Circle(11460)
+
+print('''
+medium_pizza = {}
+teaching_table = {}
+round_room = {}
+'''.format(medium_pizza.circumference(), teaching_table.circumference(), round_room.circumference()))
